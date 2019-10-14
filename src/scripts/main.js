395b43f2323c5b2f17fd6a8912807d9d3a85929b
Julian Garcia <garcia.j.julian@gmail.com>
2019-10-14 10:45:59 -0500

    added js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,4 +13,4 @@ const scum = takeNumber("Galactic Scum")
 console.log(scum)  // This should print "1. Galactic Scum" in the console
 
 const under = takeNumber("Underdogs")
-console.log(under)  // This should print "2. Underdogs" in the console
+console.log(under)  // This should print "2. Underdogs" in the consoe
