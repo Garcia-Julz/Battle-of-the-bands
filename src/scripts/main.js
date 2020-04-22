64e08c5e06cc80de0227033117daec5c04ab5d83
Julian Garcia <garcia.j.julian@gmail.com>
2020-04-21 22:52:34 -0500

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,34 @@
-let bandNumber = 1
+// let bandNumber = 1
 
-const takeNumber = function (bandName) {
+// const takeNumber = function (bandName) {
 
-    ticket = bandNumber++ + ". " + bandName
-    return ticket
+//     ticket = bandNumber++ + ". " + bandName
+//     return ticket
 
+// }
+
+
+// const scum = takeNumber("Galactic Scum")
+// console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+// const under = takeNumber("Underdogs")
+// console.log(under)  // This should print "2. Underdogs" in the consoe
+
+// const wannaBees = takeNumber("WannaBeatles")
+// console.log(wannaBees)  // This should print "3. WannaBeatles" in the consoe
+
+const x = myFunction(8, 3);
+
+function myFunction(a, b) {
+    return a * b
 }
 
+console.log(x)
 
-const scum = takeNumber("Galactic Scum")
-console.log(scum)  // This should print "1. Galactic Scum" in the console
+const p = mylFunction(2, 4);
 
-const under = takeNumber("Underdogs")
-console.log(under)  // This should print "2. Underdogs" in the consoe
+function mylFunction(l, m){
+    l * m
+}
 
-const wannaBees = takeNumber("WannaBeatles")
-console.log(wannaBees)  // This should print "3. WannaBeatles" in the consoe
-
-
+console.log(p)
